5e762c8b511c22939cfc34f0f47111cb454928b3
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-31 19:19:48 +0000

    Added tests for models

---
+++ todo/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from .models import Item
+
+class TestItemModel(TestCase):
+
+    def test_done_defaults_to_False(self):
+        item = Item(name="Item")
+        item.save()
+        self.assertEqual(item.name, "Item")
+        self.assertFalse(item.done)
+        
+    def test_can_create_an_item_with_a_name_and_status(self):
+        item = Item(name="Item", done=True)
+        item.save()
+        self.assertEqual(item.name, "Item")
+        self.assertTrue(item.done)
+    
