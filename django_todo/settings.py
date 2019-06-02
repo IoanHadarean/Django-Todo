5c55e195995c605a1ba17172219999ed5c7270ae
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-06-02 13:11:34 +0000

    Added requirements.txt and PostGre database url

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse("postgres://ngzfjuykdhmsfc:d77fb4c8ad2930ad3f6ce38f3beee53ba1ba760318aae3cacedd0ac642037b88@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/d2akjjto6jbbih")}
 
 
 # Password validation
